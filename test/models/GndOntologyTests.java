5338c475eb220fbdb21ee8fd716e20f708cc9fde
Fabian Steeg <steeg@hbz-nrw.de>
2018-04-05 11:21:09 +0200

    Use `Parameterized` for GndOntologyTests
    Working on https://github.com/hbz/lobid-gnd/issues/59

--- test/models/GndOntologyTests.java
+++ test/models/GndOntologyTests.java
@@ -4,67 +4,44 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class GndOntologyTests {
 
-	@Test
-	public void testClassLabelShort() throws FileNotFoundException {
-		assertThat(GndOntology.label("CollectiveManuscript"), equalTo("Sammelhandschrift"));
+	@Parameters(name = "{0} -> {1}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { //
+				{ "CollectiveManuscript", "Sammelhandschrift" }, //
+				{ "https://d-nb.info/standards/elementset/gnd#CollectiveManuscript", "Sammelhandschrift" }, //
+				{ "broaderTermPartitive", "Oberbegriff partitiv" }, //
+				{ "preferredName", "Bevorzugter Name" }, //
+				{ "gndIdentifier", "GND-Nummer" }, //
+				{ "ZZ", "Sonstiges Ausland, Unbekanntes Land" }, //
+				{ "http://d-nb.info/standards/vocab/gnd/gnd-sc#ZZ", "Sonstiges Ausland, Unbekanntes Land" }, //
+				{ "36", "Basteln, Handarbeiten, Heimwerken" }, //
+				{ "http://d-nb.info/standards/vocab/gnd/geographic-area-code#36", "Basteln, Handarbeiten, Heimwerken" }, //
+				{ "notKnown", "Unbekannt" }, //
+				{ "http://d-nb.info/standards/vocab/gnd/gender#notKnown", "Unbekannt" } });
+	}
+
+	private String id;
+	private String label;
+
+	public GndOntologyTests(String id, String label) {
+		this.id = id;
+		this.label = label;
 	}
 
 	@Test
-	public void testClassLabelFull() throws FileNotFoundException {
-		assertThat(GndOntology.label("https://d-nb.info/standards/elementset/gnd#CollectiveManuscript"),
-				equalTo("Sammelhandschrift"));
-	}
-
-	@Test
-	public void testObjectPropertyLabel() throws FileNotFoundException {
-		assertThat(GndOntology.label("broaderTermPartitive"), equalTo("Oberbegriff partitiv"));
-	}
-
-	@Test
-	public void testAnnotationPropertyLabel() throws FileNotFoundException {
-		assertThat(GndOntology.label("preferredName"), equalTo("Bevorzugter Name"));
-	}
-
-	@Test
-	public void testDatatypePropertyLabel() throws FileNotFoundException {
-		assertThat(GndOntology.label("gndIdentifier"), equalTo("GND-Nummer"));
-	}
-
-	@Test
-	public void testSubjectCategoryShort() {
-		assertThat(GndOntology.label("ZZ"), equalTo("Sonstiges Ausland, Unbekanntes Land"));
-	}
-
-	@Test
-	public void testSubjectCategoryFull() {
-		assertThat(GndOntology.label("http://d-nb.info/standards/vocab/gnd/gnd-sc#ZZ"),
-				equalTo("Sonstiges Ausland, Unbekanntes Land"));
-	}
-
-	@Test
-	public void testGeographicAreaCodeShort() {
-		assertThat(GndOntology.label("36"), equalTo("Basteln, Handarbeiten, Heimwerken"));
-	}
-
-	@Test
-	public void testGeographicAreaCodeFull() {
-		assertThat(GndOntology.label("http://d-nb.info/standards/vocab/gnd/geographic-area-code#36"),
-				equalTo("Basteln, Handarbeiten, Heimwerken"));
-	}
-
-	@Test
-	public void testGenderShort() throws FileNotFoundException {
-		assertThat(GndOntology.label("notKnown"), equalTo("Unbekannt"));
-	}
-
-	@Test
-	public void testGenderFull() throws FileNotFoundException {
-		assertThat(GndOntology.label("http://d-nb.info/standards/vocab/gnd/gender#notKnown"), equalTo("Unbekannt"));
+	public void testLabel() throws FileNotFoundException {
+		assertThat(GndOntology.label(id), equalTo(label));
 	}
 
 }
