99cf813f40da1d9b9f7f478b9141186201513996
Fabian Steeg <steeg@hbz-nrw.de>
2021-01-22 11:55:51 +0100

    Update README and config, add baseline.sh
    Script is work in progress, will fully test on next baseline
    update, will need email setup if/when added to crontab.
    
    See https://github.com/hbz/lobid-gnd/issues/255

---
+++ baseline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -uo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# Call on server like: setsid nohup bash baseline.sh > baseline.log 2>&1 &
+
+# details and defaults are configured in conf/application.conf
+
+export TODAY=$(date +'%Y%m%d')
+
+# get entityfacts baseline file
+cd data/entityfacts/
+wget https://data.dnb.de/opendata/authorities_entityfacts.jsonld.gz
+gunzip < authorities_entityfacts.jsonld.gz > authorities_entityfacts.jsonld
+cd ../..
+
+# index entityfacts JSON
+sbt \
+  -Dindex.entityfacts.index=entityfacts_$TODAY \
+  "runMain apps.Index entityfacts" \
+  > IndexEntityfacts_$TODAY.log 2>&1 &
+
+# clean up entityfacts baseline file
+mv data/entityfacts/authorities_entityfacts.jsonld.gz data/entityfacts/authorities_entityfacts_$TODAY.jsonld.gz
+
+# get gnd_lds baseline files
+cd data/gnd_lds
+wget https://data.dnb.de/opendata/authorities-{geografikum,koerperschaft,kongress,person,sachbegriff,werk}_lds.rdf.gz
+cd ../..
+mkdir data/index/gnd_lds_$TODAY
+
+# convert RDF_XML to JSON lines
+sbt \
+  -Dindex.entityfacts.index=entityfacts_$TODAY \
+  -Dindex.prod.name=gnd_$TODAY \
+  -Ddata.jsonlines=data/index/gnd_lds_$TODAY \
+  -Dindex.delete.baseline=GND-deprecated-baseline_$TODAY.txt \
+  "runMain apps.ConvertBaseline" \
+  > ConvertBaseline_$TODAY.log 2>&1 &
+
+# clean up gnd_lds baseline files
+mkdir data/gnd_lds/gnd_lds_$TODAY
+mv data/gnd_lds/authorities-*_lds.rdf.gz data/gnd_lds/gnd_lds_$TODAY
+
+# index JSON lines
+sbt \
+  -Dindex.prod.name=gnd_$TODAY \
+  -Ddata.jsonlines=data/index/gnd_lds_$TODAY \
+  -Dindex.delete.baseline=GND-deprecated-baseline_$TODAY.txt \
+  "runMain apps.Index baseline" \
+  > IndexBaseline_$TODAY.log 2>&1 &
