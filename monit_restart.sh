8ce998bb009fdc483f3b68c4ec502fa6ca660cc7
Pascal Christoph <christoph@hbz-nrw.de>
2021-06-25 10:06:42 +0200

    Add monit restart script

---
+++ monit_restart.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+USAGE="<GIT REPO NAME> {start|stop} <PORT> [<JAVA OPTS>]"
+
+if [ $# -lt 3 ]; then
+	echo "$USAGE
+				THIS SCRIPT SHOULD ONLY BE USED BY -MONIT-!
+
+				If you want to restart an instance, use ./restart.sh
+
+				First 3 parameters are mandatory.
+				Don't forget that the process is monitored by 'monit'.
+				It will restart automatically if you stop the API.
+				If you want to stop it permanently, do 'sudo /etc/ini.d/monit stop' first.
+				"
+	exit 65
+fi
+
+REPO=$1
+ACTION=$2
+PORT=$3
+JAVA_OPTS="$4"
+
+HOME="/home/sol"
+
+# it is important to set the proper locale
+. $HOME/.locale
+JAVA_OPTS=$(echo "$JAVA_OPTS" |sed 's#,#\ #g')
+
+cd $HOME/git/$REPO
+case $ACTION in
+	start)
+		if [ -f target/universal/stage/RUNNING_PID ]; then
+			kill $(cat target/universal/stage/RUNNING_PID)
+		fi
+		JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError" $HOME/activator-dist-1.3.5/activator "start $PORT"
+		;;
+	stop)
+		if [ -f target/universal/stage/RUNNING_PID ]; then
+			kill $(cat target/universal/stage/RUNNING_PID)
+		fi
+		;;
+	*)
+		echo "usage: $USAGE"
+		;;
+esac
+exit 0
