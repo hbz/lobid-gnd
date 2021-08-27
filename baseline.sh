c0cd807110f115f2c722124ab3b65c2c4cfe52f6
Fabian Steeg <steeg@hbz-nrw.de>
2021-08-27 11:11:40 +0200

    Tweak baseline.sh script after first complete run (#255)
    Don't depend on local config file setup, use separate backup dir

--- baseline.sh
+++ baseline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -uo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
-# Call on server like: setsid nohup bash baseline.sh > baseline.log 2>&1 &
+# Call on server sol@quaoar1:~/git/lobid-gnd$ setsid nohup bash baseline.sh > baseline.log 2>&1 &
 
 # details and defaults are configured in conf/application.conf
 
@@ -29,6 +29,7 @@ mkdir data/index/gnd_lds_$TODAY
 
 # convert RDF_XML to JSON lines
 sbt \
+  -Ddata.rdfmxl=data/gnd_lds \
   -Dindex.entityfacts.index=entityfacts_$TODAY \
   -Dindex.prod.name=gnd_$TODAY \
   -Ddata.jsonlines=data/index/gnd_lds_$TODAY \
@@ -37,8 +38,8 @@ sbt \
   > ConvertBaseline_$TODAY.log 2>&1
 
 # clean up gnd_lds baseline files
-mkdir data/gnd_lds/gnd_lds_$TODAY
-mv data/gnd_lds/authorities-*_lds.rdf.gz data/gnd_lds/gnd_lds_$TODAY
+mkdir data/backup/gnd_lds/gnd_lds_$TODAY
+mv data/gnd_lds/authorities-*_lds.rdf.gz data/backup/gnd_lds/gnd_lds_$TODAY
 
 # index JSON lines
 sbt \
@@ -49,7 +50,7 @@ sbt \
   > IndexBaseline_$TODAY.log 2>&1
 
 # index updates since last baseline (currently manual process)
-# export LAST_BASE=20201013 # get date from https://data.dnb.de/opendata/?
+# export LAST_BASE=20201013 # automate: get date from https://data.dnb.de/opendata/
 # mkdir data/index/gnd_since_$LAST_BASE
 # cp data/backup/GND-updates_2021*.jsonl data/index/gnd_since_$LAST_BASE # etc.; alt: OAI-PMH
 # setsid nohup sbt \
@@ -59,7 +60,7 @@ sbt \
 #  "runMain apps.Index baseline" \
 #  > IndexBaseline_since_$LAST_BASE.log 2>&1 &
 
-# a more automatable alternative might be to OAI-PMH updates
+# a more automatable alternative might be to use OAI-PMH updates
 # export LAST_BASE=2020-10-13 # get date from https://data.dnb.de/opendata/?
 # sbt "runMain apps.ConvertUpdates $LAST_BASE"
 # sbt "runMain apps.Index updates"
