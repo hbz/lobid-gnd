3ed6fab8eb070e334c391c33d0d917ab6cbee8df
Fabian Steeg <steeg@hbz-nrw.de>
2023-11-21 18:07:33 +0100

    Use columns for longer biographicalOrHistoricalInformation (RPB-38)

--- app/models/AuthorityResource.java
+++ app/models/AuthorityResource.java
@@ -120,10 +120,9 @@ public class AuthorityResource {
 						false);
 				List<String> collect = stream.map(n -> n.asText().replaceAll("[δ∞]", "\"").replace('Æ', '\''))
 						.collect(Collectors.toList());
-				return "<h3>Biogramm</h3><p>" + collect.get(0) + "</p>"
-						+ collect.subList(1, collect.size()).stream()
-								.map(s -> "<h3>Biogramm <small>/ alternativ</small></h3><p>" + s + "</p>")
-								.collect(Collectors.joining());
+				return "<p>" + collect.get(0) + "</p>" + collect.subList(1, collect.size()).stream()
+						.map(s -> "<h3>Biogramm <small>/ alternativ</small></h3><p>" + s + "</p>")
+						.collect(Collectors.joining());
 			}
 		}
 		return "";
