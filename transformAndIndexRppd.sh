85b1aa63fa0cdabc61a3cb59432ae8511145092d
Fabian Steeg <steeg@hbz-nrw.de>
2024-01-26 12:13:53 +0100

    Create new index and update alias in RPPD script (RPB-109)

--- transformAndIndexRppd.sh
+++ transformAndIndexRppd.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -uo pipefail # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
+TIME=$(date "+%Y%m%d-%H%M")
+INDEX="gnd-rppd-$TIME"
+ALIAS="gnd-rppd-test"
+
 cd ../rpb
 bash transformRppd.sh
 cd -
-sbt "runMain apps.Index baseline"
+sbt -Dindex.prod.name=$INDEX "runMain apps.Index baseline"
+curl -X POST "weywot3:9200/_aliases?pretty" -H 'Content-Type: application/json' -d'
+{
+	"actions" : [
+		{ "remove" : { "index" : "*", "alias" : "'"$ALIAS"'" } },
+		{ "add" : { "index" : "'"$INDEX"'", "alias" : "'"$ALIAS"'" } }
+	]
+}
+'
 curl -S -s -o /dev/null https://rppd.lobid.org
