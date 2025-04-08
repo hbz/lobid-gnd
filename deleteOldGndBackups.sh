210748bbce3059b104be8d048bbc42116e6189fc
Phu Tu <phu.tu@hbz-nrw.de>
2025-04-08 11:05:59 +0200

    Fix deleteOldGndBackups.sh, Resolves #419
    do not delete by date from dir name
    delete by modified timestamp

--- deleteOldGndBackups.sh
+++ deleteOldGndBackups.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 set -eu
 
-cd /home/sol/git/lobid-gnd
+# This script runs daily via cron
 
-# find "newest" directory by it's name
-# extract date from directory name
-dir_date=$(find data/backup/gnd_lds/ -type d | sort | tail -1 | rev | cut -d'/' -f1 | rev | cut -d'_' -f3)
+echo "$(date) : Going to delete files older than 270 days"
+find data/backup/ -name "GND-updates*" -type f -mtime +269 -delete -print
 
-if [[ -z "$dir_date" ]]; then
-	echo "$(date) : Going to delete files older than 270 days"
-	find data/backup/ -name "GND-updates*" -type f -mtime +269 -delete -print
-
-else
-	# create temp file with $dir_date as timestamp
-	touch -t ${dir_date}0000 /tmp/$dir_date
-
-	echo "$(date) : Going to delete files older than $dir_date"
-	find data/backup/ -name "GND-updates*" -type f ! -newer /tmp/$dir_date -delete -print
-	
-	# delete temp file
-	rm /tmp/$dir_date
-fi
