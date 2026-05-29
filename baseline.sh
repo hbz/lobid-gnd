567a2ac39093882f616252b6f626c88b88c6efe4
Fabian Steeg <steeg@hbz-nrw.de>
2026-05-29 10:54:11 +0200

    Update instructions for manual updates in baseline.sh (#473)
    Remove old approach with many manual copy steps, add `--java-home`

--- baseline.sh
+++ baseline.sh
@@ -53,23 +53,13 @@ sbt --java-home $JAVA_HOME \
   "runMain apps.Index baseline" \
   > IndexBaseline_$TODAY.log 2>&1
 
-# index updates since last baseline (currently manual process)
-# export TODAY=20250404 # date used in the part above, see existing index
-# export LAST_BASE=20250313 # get date from description on https://data.dnb.de/opendata/ (e.g. "Stand: 13.03.2025")
-# mkdir data/index/gnd_since_$LAST_BASE
-# cp data/backup/GND-updates_2025-03-13.jsonl data/index/gnd_since_$LAST_BASE # etc.
-# setsid nohup sbt \
-#  -Dindex.entityfacts.index=entityfacts_$TODAY \
-#  -Dindex.prod.name=gnd_$TODAY \
-#  -Ddata.jsonlines=data/index/gnd_since_$LAST_BASE \
-#  -Dindex.delete.baseline=GND-deprecated-updates.txt \
-#  "runMain apps.Index baseline" \
-#  > IndexBaseline_since_$LAST_BASE.log 2>&1 &
+## index updates since last baseline (currently manual process)
+## use OAI-PMH updates - run on test system (quaoar13) to avoid interfering with hourly updates!
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+# export TODAY=20260527 # date used in the part above, see existing index
+# export LAST_BASE=2026-02-17T12:00:00Z # get date from description on https://data.dnb.de/opendata/ (e.g. "Stand: 17.02.2026 14:15 Uhr UTC")
+# setsid nohup sbt --java-home $JAVA_HOME -mem 4000 -Dindex.prod.name=gnd_$TODAY "runMain apps.ConvertUpdates $LAST_BASE" > ConvertUpdates_since_$LAST_BASE.log 2>&1 &
+# setsid nohup sbt --java-home $JAVA_HOME -mem 4000 -Dindex.prod.name=gnd_$TODAY "runMain apps.Index updates" > IndexUpdates_since_$LAST_BASE.log 2>&1 &
 
-# Alternative: use OAI-PMH updates - run on test system to avoid interfering with hourly updates!
-# export TODAY=20250404 # date used in the part above, see existing index
-# export LAST_BASE=2025-03-13T00:00:00Z # get date from description on https://data.dnb.de/opendata/ (e.g. "Stand: 13.03.2025")
-# setsid nohup sbt -mem 4000 -Dindex.prod.name=gnd_$TODAY "runMain apps.ConvertUpdates $LAST_BASE" > ConvertUpdates_since_$LAST_BASE.log 2>&1 &
-# setsid nohup sbt -mem 4000 -Dindex.prod.name=gnd_$TODAY "runMain apps.Index updates" > IndexUpdates_since_$LAST_BASE.log 2>&1 &
-
-## finally, switch 'gnd' alias to 'gnd_$TODAY' and 'entityfacts' alias to 'entityfacts_$TODAY'
+## on prod system (quaoar11), make sure GND-lastSuccessfulUpdate.txt is earlier than the time you just started the updates (so we don't miss updates on prod)
+## finally, switch elasticsearch 'gnd' alias to 'gnd_$TODAY' and 'entityfacts' alias to 'entityfacts_$TODAY'
